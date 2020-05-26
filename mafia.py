5ffaa9f97e53139b009179cad647b9a19863a623
maxjerdee <mjerdee@princeton.edu>
2020-05-26 00:18:35 -0400

    Notes on game/player states

--- mafia.py
+++ mafia.py
@@ -9,10 +9,14 @@ teams = ["T", "M", "C", "V", "S", "W"] # Town, Mafia, Coven, Vampires, Serial Ki
 roles = [] # full role information
 role_names = [] # names of roles
 role_teams = [] # number denoting the affiliation of each role
+num = 0
 with open("roles.txt","r") as role_reader:
     for role in role_reader:
         roles.append(role.split())
         role_names.append(roles[-1][0])
+
+roles = roles[1:]
+role_names = role_names[1:]
 
 investigation_groups = [["Investigator", "Consigliere", "Mayor", "Survivor", "Plaguebearer", "Pestilence"],["Bookie", "Priest", "Vampire", "Eldest_Vampire","Jester"],\
     ["Lookout", "Escort", "Siren", "Consort", "Serial_Killer"],["Vampire_Hunter", "Amnesiac", "Medusa", "Psychic", "Disguiser"],\
@@ -32,4 +36,17 @@ role_count = len(roles)
 # Initialize game state
 # Randomly assign roles:
 print(role_count, player_count)
+uniques = ["Jailer","Spy","Veteran","Mayor","Medium","Godfather","Mafioso","Coven_Leader","Puppeteer","Plaguebearer","Juggernaut","Eros","Eldest_Vampire"]
+none = ["Pestilence"]
+known = {"Seth Lovelace":"Coven_Leader", "Irene Hsu":"Siren","Emerson Thomas":"Medusa", "Conor Rachlin":"Medusa", "Lucas Salvador":"Poisoner", "Max Jerdee":"Puppeteer", "Natalia Miller":"Hex_Master", "Cassidy Crone":"Necromaster", "Ed Horan":"Potion_Master", "Emma Moriarty":"Mirage"}
 
+randomness = 1
+
+# Suspicion Matrix for each faction
+# Player State: Role, Alive/Dead, AP, Base DP, Extra DP, Poisoned, Infected, Doused, Target, 
+# Visit_History, Jailed, RBed, Love, Trapped, Hexxed, 
+# ^ store as array of dictionaries?
+
+# Game State: Player States + Suspicion Matrices, Moon/Full Moon
+
+# Check victory conditions
