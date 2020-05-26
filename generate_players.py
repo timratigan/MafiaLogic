5a6b5139c2a77e0880942ca2b43aa3085644b9b9
maxjerdee <mjerdee@princeton.edu>
2020-05-26 05:57:31 -0400

    Thermalization Experiments

--- generate_players.py
+++ generate_players.py
@@ -1,6 +1,7 @@
 import numpy as np
 import random
 from load_data import *
+import matplotlib.pyplot as plt 
 def generate_role_counts(GOD_RANDOMNESS=0.4,MIN_ROLE=1,MAX_ROLE=5,MAX_ROLE_MULT=2,known_counts={},print_flag=False):
     # Use God expectations to produce role numbers:
     role_counts = np.zeros(num_roles,dtype=int)
@@ -19,7 +20,7 @@ def generate_role_counts(GOD_RANDOMNESS=0.4,MIN_ROLE=1,MAX_ROLE=5,MAX_ROLE_MULT=
         role_counts[role_names.index(none_role)] = 0
     for (role_name, role_count) in known_counts.items():
         role_counts[role_names.index(role_name)]=role_count
-    # We rewrite the mean so that the next step cannot mess with the known information
+        roles[role_names.index(role_name)][5] = role_count # We rewrite the mean so that the next step cannot mess with the known information
 
     # Produce the proper total number of players by adjusting the outliers back to the mean
     residues = {}
@@ -38,11 +39,67 @@ def generate_role_counts(GOD_RANDOMNESS=0.4,MIN_ROLE=1,MAX_ROLE=5,MAX_ROLE_MULT=
 
     return role_counts.astype(int)
 
-def generate_player_assignment():
+def temperature(t): # Choose the appropriate temperature for interation step t
+    return 1 + 10000*np.exp(-t/1000)
 
+def assignment_energy(assignment, sus_square): # Calculate the "energy" (- log of culmulative probability) of a given arrangement
+    probability = 1
+    chance_sum = 0
+    for player_index in range(num_players):
+        probability *= sus_square[player_index, assignment[player_index]]
+        chance_sum += sus_square[player_index, assignment[player_index]]
+    return -np.log(probability + 0*chance_sum) 
+
+def check_assignment(assignment, role_indices_extended):
+    known_roles = ["Coven_Leader", "Siren", "Siren", "Medusa", "Medusa", "Poisoner", "Puppeteer", "Hex_Master", "Necromancer", "Potion_Master", "Mirage"]
+    correct = 0
+    for player_index in range(54,65):
+        if known_roles[player_index - 54] == role_names[role_indices_extended[assignment[player_index]]]:
+            correct += 1
+    return correct
+
+def generate_player_assignment(sus_matrix, role_counts):
     # Question: Given a suspicion matrix of priors, i.e. likelihood that each player is each role, pull a player distribution
     # Answer? For each possible combination, can compute the probability given the matrix, and then sample
-    # Start with flat suspicion matrix, except for the known ones:
-    suspicion_matrix = np.ones((num_players,num_roles))/num_roles
+    # But this is prohibitively expensive (65! options), so we instead adopt a thermalization strategy, where the energy is (-log of) the culmulative probability
+
+    # Extending sus_matrix to a square
+    sus_square = np.zeros((num_players,num_players))
+    role_indices_extended = []
+    for role_index in range(num_roles):
+        for i in range(role_counts[role_index]):
+            role_indices_extended.append(role_index)
+
+    for player_index in range(num_players):
+        for extended_role_index in range(num_players):
+            sus_square[player_index,extended_role_index] = sus_matrix[player_index,role_indices_extended[extended_role_index]]
+    
+    # Thermalization 
+    # Generate Random Starting Point 
+    assignment = np.random.permutation(range(num_players))
+    current_energy = assignment_energy(assignment,sus_square)
+    positions = []
+    for t in range(10000):
+        random_index = np.random.randint(num_players,size=2) # Random index permutation
+        candidate_assignment = assignment.copy()
+        candidate_assignment[random_index[0]] = assignment[random_index[1]]
+        candidate_assignment[random_index[1]] = assignment[random_index[0]]
+        new_energy = assignment_energy(candidate_assignment,sus_square)
+        energy_difference = new_energy - current_energy
+        if np.random.random() < np.exp(-temperature(t)*energy_difference):
+            assignment = candidate_assignment
+            current_energy = new_energy
+            positions.append([t,current_energy,check_assignment(assignment,role_indices_extended)])
+    positions = np.array(positions)
+    plt.plot(positions[:,0],(positions[:,1]-250)*5)
+    plt.plot(positions[:,0],positions[:,2]*10)
+    plt.savefig("graph.png")
+    print(check_assignment(assignment,role_indices_extended))
+    # Match result with player assignment
     player_assignment = {}
+    for player_index in range(num_players):
+        player_assignment.update({players[player_index]:role_names[role_indices_extended[assignment[player_index]]]})
+    
     return player_assignment
+
+
--- mafia.py
+++ mafia.py
@@ -3,10 +3,12 @@ import re
 from extra_functions import *
 from load_data import *
 from generate_players import *
+import matplotlib.pyplot as plt
 
-#random.seed(0)
+random.seed(0)
+np.random.seed(0)
 # Known information
-known = {"Seth Lovelace":"Coven_Leader", "Irene Hsu":"Siren","Emerson Thomas":"Medusa", "Conor Rachlin":"Medusa", "Lucas Salvador":"Poisoner", "Max Jerdee":"Puppeteer", "Natalia Miller":"Hex_Master", "Cassidy Crone":"Necromancer", "Ed Horan":"Potion_Master", "Emma Moriarty":"Mirage"} 
+known_assignments = {"Seth Lovelace":"Coven_Leader", "Irene Hsu":"Siren","AJ Kawczynski":"Siren","Emerson Thomas":"Medusa", "Conor Rachlin":"Medusa", "Lucas Salvador":"Poisoner", "Max Jerdee":"Puppeteer", "Natalia Miller":"Hex_Master", "Cassidy Crone":"Necromancer", "Ed Horan":"Potion_Master", "Emma Moriarty":"Mirage"} 
 known_counts = {"Siren":2,"Medusa":2,"Poisoner":1,"Hex_Master":1,"Necromancer":1,"Potion_Master":1,"Mirage":1}
 
 GOD_RANDOMNESS = 0.5 # Assume that each role appears with \pm GOD_RANDOMNESS*100 %. 
@@ -14,6 +16,7 @@ MIN_ROLE = 0 # Option to force a minimum number of players in each role
 MAX_ROLE = 5 # Option to force a maximum number of players in each role
 MAX_ROLE_MULT = 2 # Option to force a maximum number of players in each role as a multiple of the average for the role
 
+""" Testing Role Count Generation
 for t in range(10):
     role_counts = generate_role_counts(GOD_RANDOMNESS,MIN_ROLE,MAX_ROLE,MAX_ROLE_MULT,known_counts)
     anomalies = {}
@@ -21,7 +24,30 @@ for t in range(10):
         if role_counts[role_index] != 1:
             anomalies.update({role_names[role_index]:role_counts[role_index]})
     print(anomalies)
+"""
+role_counts = generate_role_counts(GOD_RANDOMNESS,MIN_ROLE,MAX_ROLE,MAX_ROLE_MULT,known_counts)
 
+# Start with flat sus_matrix
+sus_matrix = np.ones((num_players,num_roles))/num_roles + 0.01*np.random.rand(num_players,num_roles) # Matrix containing suspicions, sus_matrix[player,role] is % chance player is role 
+
+# Hint sus_matrix with known roles
+for known_name, known_role in known_assignments.items():
+    sus_matrix[players.index(known_name),role_names.index(known_role)]*=2
+
+"""
+# Update sus_matrix with known roles
+for known_name, known_role in known_assignments.items():
+    sus_matrix[players.index(known_name),]=np.zeros(num_roles)
+    sus_matrix[players.index(known_name),role_names.index(known_role)]=1
+"""
+
+# Normalize
+for player_index in range(num_players):
+    sus_matrix[player_index,] /= np.sum(sus_matrix[player_index,])
+
+player_assignment = generate_player_assignment(sus_matrix, role_counts)
+
+print(player_assignment)
 # Notes from Seth:
 # Suspicion Matrix for each faction 
 # Player State: Role, Alive/Dead, AP, Base DP, Extra DP, Poisoned, Infected, Doused, Target, 
